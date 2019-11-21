c2c79444f208060c1b8884a94e11d37722b020c3
Анастасия Лещенко <leshencko.nastia2013@gmail.com>
2019-11-21 16:02:21 +0300

    add login page

---
+++ src/styles/LoginPage.css.js
@@ -0,0 +1,30 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme();
+
+export default {
+
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    container: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+
+}
